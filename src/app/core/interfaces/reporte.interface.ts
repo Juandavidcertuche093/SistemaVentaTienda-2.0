77dc0e398fdeb03b090b314735410f300a00c83c
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2025-05-07 16:07:18 -0500

    implement sales reporting module

---
+++ src/app/core/interfaces/reporte.interface.ts
@@ -0,0 +1,12 @@
+export interface Reporte {
+  numeroVenta:string,
+  tipoPago:string,
+  fechaRegistro:string,
+  totalVenta:string,
+  nombreProducto:string,
+  presentacion:string,
+  cantidad:number,
+  precio:string,
+  total:string,
+  usuario:string
+}
